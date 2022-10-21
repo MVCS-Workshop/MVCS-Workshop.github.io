a3d4b810873603cc453ee570cf39c04e0081410c
Yining Hong <yninghong@gmail.com>
2022-10-21 13:08:40 -0700

    Update timezone.js

--- js/timezone.js
+++ js/timezone.js
@@ -14,74 +14,74 @@ function setTimezone(tz) {
         minute: '2-digit'
     }
     schedule = {
-        "#intro_start": new Date(Date.parse("23 Oct 08:45:00 GMT+0200 (Israel Standard Time)")),
-        "#intro_end": new Date(Date.parse("23 Oct 09:00:00 GMT+0200 (Israel Standard Time)")),
+        "#intro_start": new Date(Date.parse("23 Oct 08:45:00 GMT+0300 (Israel Standard Time)")),
+        "#intro_end": new Date(Date.parse("23 Oct 09:00:00 GMT+0300 (Israel Standard Time)")),
 
-        "#sec1_talk1_start": new Date(Date.parse("23 Oct 09:00:00 GMT+0200 (Israel Standard Time)")),
-        "#sec1_talk1_end": new Date(Date.parse("23 Oct 09:30:00 GMT+0200 (Israel Standard Time)")),
+        "#sec1_talk1_start": new Date(Date.parse("23 Oct 09:00:00 GMT+0300 (Israel Standard Time)")),
+        "#sec1_talk1_end": new Date(Date.parse("23 Oct 09:30:00 GMT+0300 (Israel Standard Time)")),
 
-        "#sec1_talk2_start": new Date(Date.parse("23 Oct 09:30:00 GMT+0200 (Israel Standard Time)")),
-        "#sec1_talk2_end": new Date(Date.parse("23 Oct 10:00:00 GMT+0200 (Israel Standard Time)")),
+        "#sec1_talk2_start": new Date(Date.parse("23 Oct 09:30:00 GMT+0300 (Israel Standard Time)")),
+        "#sec1_talk2_end": new Date(Date.parse("23 Oct 10:00:00 GMT+0300 (Israel Standard Time)")),
 
-        "#sec1_talk3_start": new Date(Date.parse("23 Oct 10:00:00 GMT+0200 (Israel Standard Time)")),
-        "#sec1_talk3_end": new Date(Date.parse("23 Oct 10:30:00 GMT+0200 (Israel Standard Time)")),
+        "#sec1_talk3_start": new Date(Date.parse("23 Oct 10:00:00 GMT+0300 (Israel Standard Time)")),
+        "#sec1_talk3_end": new Date(Date.parse("23 Oct 10:30:00 GMT+0300 (Israel Standard Time)")),
 
-        "#break1_start": new Date(Date.parse("23 Oct 10:30:00 GMT+0200 (Israel Standard Time)")),
-        "#break1_end": new Date(Date.parse("23 Oct 10:45:00 GMT+0200 (Israel Standard Time)")),
+        "#break1_start": new Date(Date.parse("23 Oct 10:30:00 GMT+0300 (Israel Standard Time)")),
+        "#break1_end": new Date(Date.parse("23 Oct 10:45:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge1_start": new Date(Date.parse("23 Oct 10:45:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge1_end": new Date(Date.parse("23 Oct 11:05:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge1_start": new Date(Date.parse("23 Oct 10:45:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge1_end": new Date(Date.parse("23 Oct 11:05:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge1_public_start": new Date(Date.parse("23 Oct 11:05:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge1_public_end": new Date(Date.parse("23 Oct 11:25:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge1_public_start": new Date(Date.parse("23 Oct 11:05:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge1_public_end": new Date(Date.parse("23 Oct 11:25:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge1_private_start": new Date(Date.parse("23 Oct 11:25:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge1_private_end": new Date(Date.parse("23 Oct 11:45:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge1_private_start": new Date(Date.parse("23 Oct 11:25:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge1_private_end": new Date(Date.parse("23 Oct 11:45:00 GMT+0300 (Israel Standard Time)")),
 
-        "#lunch_start": new Date(Date.parse("23 Oct 11:45:00 GMT+0200 (Israel Standard Time)")),
-        "#lunch_end": new Date(Date.parse("23 Oct 12:20:00 GMT+0200 (Israel Standard Time)")),
+        "#lunch_start": new Date(Date.parse("23 Oct 11:45:00 GMT+0300 (Israel Standard Time)")),
+        "#lunch_end": new Date(Date.parse("23 Oct 12:20:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge2_start": new Date(Date.parse("23 Oct 12:20:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge2_end": new Date(Date.parse("23 Oct 12:40:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge2_start": new Date(Date.parse("23 Oct 12:20:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge2_end": new Date(Date.parse("23 Oct 12:40:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge2_talk_start": new Date(Date.parse("23 Oct 12:40:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge2_talk_end": new Date(Date.parse("23 Oct 13:00:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge2_talk_start": new Date(Date.parse("23 Oct 12:40:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge2_talk_end": new Date(Date.parse("23 Oct 13:00:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge2_leaderboard_talk1_start": new Date(Date.parse("23 Oct 13:00:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge2_leaderboard_talk1_end": new Date(Date.parse("23 Oct 13:20:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge2_leaderboard_talk1_start": new Date(Date.parse("23 Oct 13:00:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge2_leaderboard_talk1_end": new Date(Date.parse("23 Oct 13:20:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge2_leaderboard_talk2_start": new Date(Date.parse("23 Oct 13:20:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge2_leaderboard_talk2_end": new Date(Date.parse("23 Oct 13:40:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge2_leaderboard_talk2_start": new Date(Date.parse("23 Oct 13:20:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge2_leaderboard_talk2_end": new Date(Date.parse("23 Oct 13:40:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge3_start": new Date(Date.parse("23 Oct 13:40:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge3_end": new Date(Date.parse("23 Oct 14:00:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge3_start": new Date(Date.parse("23 Oct 13:40:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge3_end": new Date(Date.parse("23 Oct 14:00:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge4_start": new Date(Date.parse("23 Oct 14:00:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge4_end": new Date(Date.parse("23 Oct 14:20:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge4_start": new Date(Date.parse("23 Oct 14:00:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge4_end": new Date(Date.parse("23 Oct 14:20:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge4_talk_start": new Date(Date.parse("23 Oct 14:20:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge4_talk_end": new Date(Date.parse("23 Oct 14:40:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge4_talk_start": new Date(Date.parse("23 Oct 14:20:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge4_talk_end": new Date(Date.parse("23 Oct 14:40:00 GMT+0300 (Israel Standard Time)")),
 
-        "#break2_start": new Date(Date.parse("23 Oct 14:40:00 GMT+0200 (Israel Standard Time)")),
-        "#break2_end": new Date(Date.parse("23 Oct 14:45:00 GMT+0200 (Israel Standard Time)")),
+        "#break2_start": new Date(Date.parse("23 Oct 14:40:00 GMT+0300 (Israel Standard Time)")),
+        "#break2_end": new Date(Date.parse("23 Oct 14:45:00 GMT+0300 (Israel Standard Time)")),
 
-        "#sec2_talk1_start": new Date(Date.parse("23 Oct 14:45:00 GMT+0200 (Israel Standard Time)")),
-        "#sec2_talk1_end": new Date(Date.parse("23 Oct 15:15:00 GMT+0200 (Israel Standard Time)")),
+        "#sec2_talk1_start": new Date(Date.parse("23 Oct 14:45:00 GMT+0300 (Israel Standard Time)")),
+        "#sec2_talk1_end": new Date(Date.parse("23 Oct 15:15:00 GMT+0300 (Israel Standard Time)")),
 
-        "#sec2_talk2_start": new Date(Date.parse("23 Oct 15:15:00 GMT+0200 (Israel Standard Time)")),
-        "#sec2_talk2_end": new Date(Date.parse("23 Oct 15:45:00 GMT+0200 (Israel Standard Time)")),
+        "#sec2_talk2_start": new Date(Date.parse("23 Oct 15:15:00 GMT+0300 (Israel Standard Time)")),
+        "#sec2_talk2_end": new Date(Date.parse("23 Oct 15:45:00 GMT+0300 (Israel Standard Time)")),
 
-        "#sec2_talk3_start": new Date(Date.parse("23 Oct 15:45:00 GMT+0200 (Israel Standard Time)")),
-        "#sec2_talk3_end": new Date(Date.parse("23 Oct 16:15:00 GMT+0200 (Israel Standard Time)")),
+        "#sec2_talk3_start": new Date(Date.parse("23 Oct 15:45:00 GMT+0300 (Israel Standard Time)")),
+        "#sec2_talk3_end": new Date(Date.parse("23 Oct 16:15:00 GMT+0300 (Israel Standard Time)")),
 
-        "#break3_start": new Date(Date.parse("23 Oct 16:15:00 GMT+0200 (Israel Standard Time)")),
-        "#break3_end": new Date(Date.parse("23 Oct 16:20:00 GMT+0200 (Israel Standard Time)")),
+        "#break3_start": new Date(Date.parse("23 Oct 16:15:00 GMT+0300 (Israel Standard Time)")),
+        "#break3_end": new Date(Date.parse("23 Oct 16:20:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge5_start": new Date(Date.parse("23 Oct 16:20:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge5_end": new Date(Date.parse("23 Oct 16:40:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge5_start": new Date(Date.parse("23 Oct 16:20:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge5_end": new Date(Date.parse("23 Oct 16:40:00 GMT+0300 (Israel Standard Time)")),
 
-        "#challenge6_start": new Date(Date.parse("23 Oct 16:40:00 GMT+0200 (Israel Standard Time)")),
-        "#challenge6_end": new Date(Date.parse("23 Oct 17:00:00 GMT+0200 (Israel Standard Time)")),
+        "#challenge6_start": new Date(Date.parse("23 Oct 16:40:00 GMT+0300 (Israel Standard Time)")),
+        "#challenge6_end": new Date(Date.parse("23 Oct 17:00:00 GMT+0300 (Israel Standard Time)")),
 
     }
     // blocks = {
